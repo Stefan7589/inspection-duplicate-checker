a6a11ec8148031a3daf9e570d0db6aca11e22ff8
Stefan7589 <s.conradie@vestrel.com>
2025-11-20 11:46:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -112,4 +112,5 @@ if st.button("Run Duplicate Check"):
             cols = st.columns(len(group))
             for col, (_, row) in zip(cols, group.iterrows()):
                 col.markdown(f"**{row['file']} — Page {row['page']}**")
-                col.image(row["image"], use_column_width=50)
+                col.image(row["image"], width=50)
+
